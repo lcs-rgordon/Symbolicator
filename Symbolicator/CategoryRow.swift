a903c224a88946eeec2c7ba2c67335c730960724
Russell Gordon <github@russellgordon.ca>
2022-06-26 12:30:11 -0400

    Got basic list view working.

---
+++ Symbolicator/CategoryRow.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryRow.swift
+//  Symbolicator
+//
+//  Created by Russell Gordon on 2022-06-26.
+//
+
+import SwiftUI
+
+// Handles one row in our list
+struct CategoryRow: View {
+    
+    let category: Category
+    
+    var body: some View {
+        // When I'm tapped, select that category
+        NavigationLink(value: category) {
+            // Show the category name and icon
+            Label(category.name, systemImage: category.icon)
+        }
+    }
+}
+
+//struct CategoryRow_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CategoryRow(category: example)
+//    }
+//}
--- Symbolicator/ContentView.swift
+++ Symbolicator/ContentView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(viewModel.specialCategories) { category in
+                        CategoryRow(category: category)
+                    }
+                }
+                
+                Section {
+                    ForEach(viewModel.regularCategories) { category in
+                        CategoryRow(category: category)
+                    }
+                }
+
+            }
+            .navigationTitle("Symbolicator")
         }
+        .environmentObject(viewModel)
     }
 }
 
