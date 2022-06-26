50f27a82ef7e95c8c29793c13cc535b584a16da7
Russell Gordon <github@russellgordon.ca>
2022-06-26 12:07:59 -0400

    Initial Commit

---
+++ Symbolicator/SymbolicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolicatorApp.swift
+//  Symbolicator
+//
+//  Created by Russell Gordon on 2022-06-26.
+//
+
+import SwiftUI
+
+@main
+struct SymbolicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
